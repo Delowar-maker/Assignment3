37ddb8c54f7303622d9648c1a9ddcc563e32ca4c
Delowar-maker <56905688+Delowar-maker@users.noreply.github.com>
2023-06-19 14:44:50 +0600

    DOM Query Selector
    DOM Query Selector

---
+++ 13 DOM Query Selector/dom.js
@@ -0,0 +1,8 @@
+function changeColor() {
+  var paragraphs = document.querySelectorAll(".class1");
+  for (var i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = "red";
+  }
+}
+
+changeColor();
